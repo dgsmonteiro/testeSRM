d15ffd83fe33085fe1cf17081731df8e76f7c385
Douglas Monteiro <douglasmonteiro@icloud.com>
2019-02-06 18:19:25 -0200

    corrigindo erro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SelectSrmComponent } from './components/select-srm/select-srm.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { SelectAcoesService } from './services/select-acoes.service';
+import { PagesModule } from './pages/pages.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ItemMenuNavBarModule } from './components/item-menu-nav-bar/item-menu-nav-bar.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectSrmComponent,
+    HeaderComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    ItemMenuNavBarModule,
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule
   ],
-  providers: [],
+  providers: [SelectAcoesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
